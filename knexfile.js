d1bd779a1b362cb12f27f795523034e916b554c4
Ellen Longley <ellenharperlongley@gmail.com>
2022-10-25 18:59:07 -0400

    add production config

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,18 @@ module.exports = {
     },
   },
 
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
   test: {
     client: "sqlite3",
     connection: {
